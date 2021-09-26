c395b89270a86d28963fb8e420cf60f470d2e515
N.Kishino <naopon2009@gmail.com>
2021-09-26 15:47:58 +0900

    　とりあえず動くよね

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="login_form">
-
     <div class="login_input">
-      <div id="login_title">ログイン
+      <div id="login_title">
+        ログイン
         <div id="greet_message">
-        おかえりなさいまし。
-      </div>
-
+          おかえりなさいまし。
+        </div>
       </div>
 
       <ul class="login_form_input_li">
         <li>メールアドレス</li>
-        <li><input type="text" class="login_input" v-model="user_email"/></li>
+        <li><input type="text" class="login_input" v-model="user_email" /></li>
 
         <li>パスワード</li>
-        <li><input type="password" class="login_input" v-model="user_pass"/></li>
+        <li>
+          <input type="password" class="login_input" v-model="user_pass" />
+        </li>
 
         <li>
           <div>
-
-          <button  id="button_login_submit" @click="request_login()">ログイン</button>
-          <a>パスワード忘れた…</a>
-        </div>
+            <button id="button_login_submit" @click="request_login()">
+              ログイン
+            </button>
+            <a>パスワード忘れた…</a>
+          </div>
         </li>
-
       </ul>
-
     </div>
   </div>
 </template>
@@ -45,16 +45,14 @@ export default {
   },
   methods: {
     request_login () {
-      axios.post('/login', {
-        email_user: this.user_email,
-        pass: this.user_pass
-      }
-      ).then(response => {
+      const params = new URLSearchParams()
+      params.append('mail_address', this.user_email)
+      params.append('password', this.user_pass)
+      axios.post('/login', params).then(response => {
         // タスクリストにとりあえず飛ばす
-        window.location.href = '/tasklist'
+        this.router.push('/tasklist')
       })
     }
   }
-
 }
 </script>
