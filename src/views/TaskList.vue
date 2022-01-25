d712f48ae4a5322030bc36850cc0da14f57e763c
oyaki <whitemisosoup2001@gmail.com>
2022-01-25 12:38:41 +0900

    ref追加

--- src/views/TaskList.vue
+++ src/views/TaskList.vue
@@ -1,37 +1,37 @@
 <template>
-    <div class="task_page">
-      <!-- 題名部分 -->
-      <div id="task_add_date">{{task_date}}</div>
-      <!-- メイン部分  -->
-      <div id="main_task_lists">
-        <!-- タスク入力欄 -->
-        <Taskinput></Taskinput>
+  <div class="task_page">
+    <!-- 題名部分 -->
+    <div id="task_add_date">{{ task_date }}</div>
+    <!-- メイン部分  -->
+    <div id="main_task_lists">
+      <!-- タスク入力欄 -->
+      <Taskinput ref="taskInputCompleted"></Taskinput>
 
-        <div v-for="itemProp in tasklist" v-bind:key="itemProp.id">
-          <Taskplan :item='itemProp'></Taskplan>
-        </div>
-      </div>
-      <div id="right_side_menu">
-        <ul
-          id="right-sidebar_list"
-          class="nav-sidebar"
-          style="list-style: none; padding-left: 0"
-        >
-          <li>
-            <TaskCalendar></TaskCalendar>
-          </li>
-          <li class="mt-5">
-            <TaskListClock></TaskListClock>
-          </li>
-
-          <li class="mt-2">
-            <button type="button" id="begin_work_button" class="btn_danger">
-              作業開始！
-            </button>
-          </li>
-        </ul>
+      <div v-for="itemProp in tasklist" v-bind:key="itemProp.id">
+        <Taskplan :item="itemProp"></Taskplan>
       </div>
     </div>
+    <div id="right_side_menu">
+      <ul
+        id="right-sidebar_list"
+        class="nav-sidebar"
+        style="list-style: none; padding-left: 0"
+      >
+        <li>
+          <TaskCalendar></TaskCalendar>
+        </li>
+        <li class="mt-5">
+          <TaskListClock></TaskListClock>
+        </li>
+
+        <li class="mt-2">
+          <button type="button" id="begin_work_button" class="btn_danger">
+            作業開始！
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped src="@/assets/sass/task_lists.scss"></style>
 <script>
@@ -40,7 +40,6 @@ import Taskplan from '@/components/Tasks/TaskPlans.vue'
 import TaskListClock from '@/components/Clocks/TasklistClock.vue'
 import TaskCalendar from '@/components/Calendar/TaskCalendar.vue'
 export default {
-
   components: {
     Taskinput,
     Taskplan,
@@ -49,7 +48,6 @@ export default {
   },
   data: function () {
     return {
-
       DatePickerFormat: 'yyyy-MM-dd',
       checkdone: [],
       tasklist: [],
@@ -57,26 +55,21 @@ export default {
       modelConfig: {
         type: 'string'
       }
-
     }
   },
   methods: {
-
+    taskInputCompleted () {}
   },
   // DOMが出来上がる前にやっとく処理
   created: function () {
     this.$http
       .get('/tasks', { params: { pageNumber: this.pageNumber } })
-      .then((response) => (this.tasklist = response.data.result))
+      .then(response => (this.tasklist = response.data.result))
   },
 
   // DOMが出来上がった時点の処理
-  mounted () {
-
-  },
+  mounted () {},
   // 値の変更監視
-  watch () {
-
-  }
+  watch () {}
 }
 </script>
